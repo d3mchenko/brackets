a087aca58f33daa1285db3a8a834c8b52471830d
d3mchenko <maksim-ignatyuk@mail.ru>
2021-03-16 01:15:33 +0300

    fix: update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  while (true) {
+    let flag = false;
+    bracketsConfig.forEach (element => {
+      if (str.includes(element[0] + element[1])) {
+        str = str.replace(element[0] + element[1], '')
+        flag = true;
+      }
+    })
+    if (!flag) {
+      break;
+    }
+  }
+  return (str == 0) ? true : false;
 }
